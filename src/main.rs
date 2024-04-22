3e9e10c718fedf729d16bf8f78aa53ffff9d43a3
pasit.sri <ong.pasit@gmail.com>
2024-04-22 10:40:55 +0700

    get content in medium

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::{borrow::Cow, str::FromStr};
 
+use regex::Regex;
 use reqwest::header::{self, HeaderValue, InvalidHeaderValue};
+use serde::de::Error;
 use thiserror::Error;
 
 #[derive(Debug)]
@@ -35,6 +37,12 @@ enum ClientError {
 
     #[error("failed to build client")]
     BuildError(reqwest::Error),
+
+    #[error("failed to use regex")]
+    RegexError(regex::Error),
+
+    #[error("Not found")]
+    MissMatch,
 }
 
 impl<'a> MediumClient<'a> {
@@ -81,14 +89,27 @@ impl<'a> MediumClient<'a> {
 
         Ok(result)
     }
+
+    async fn get_content(data: Data) -> Result<String, ClientError> {
+        let text = r#"text":\s*"((?:[^"\\]|\\.)*)"#;
+        let re = Regex::new(text).map_err(ClientError::RegexError).unwrap();
+        let mut m = vec![];
+        for (_, [out]) in re.captures_iter(&data.body).map(|c| c.extract()) {
+            m.push(out);
+        }
+        println!("{:?}", m);
+        Ok("wow".to_owned())
+    }
 }
 
 #[tokio::main]
 async fn main() {
     let cookie = r#"uid=1f4ef4ec3d88; sid=1:l17QxZDJ+77kzz9TCbsyWzGhy9sQLuCYulwUNpS8YZWA7R90lZDHutwneJSMz+Tg;"#;
     let client = MediumClient::new(cookie).unwrap();
-    let url = "https://medium.com/towards-data-science/how-to-grow-your-career-without-feeling-stuck-c7fe96881a44";
+    let url = "https://medium.com/towards-data-science/fine-tune-llama-3-with-orpo-56cfab2f9ada";
     let output = client.fetch(url).await.unwrap();
+    //
+    // println!("{:#?}", output);
 
-    println!("{:#?}", output);
+    MediumClient::get_content(output).await.unwrap();
 }
